9fc882f47e9571a5ff68cd47198e5d25834b6df3
crocovert <patrick.palmier@cerema.fr>
2026-04-24 14:30:40 +0200

    1.0.17

--- obj/Release/net8.0/win-x64/Muslix.AssemblyInfo.cs
+++ obj/Release/net8.0/win-x64/Muslix.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Muslix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.10.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.10+1043001a5e6b8deaa15256eac03a7ff434952899")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.10+6021cc7c1489a6c3e4137188c6b2e8f9d870ba7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Muslix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Muslix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.10.0")]
